40e241679949235d91adcc68361b868653a994bc
Uliana Seredenko <uliana1331@gmail.com>
2024-10-27 14:52:30 +0100

    start the api fn

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,43 @@
+//Imports
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const key = '41103551-afb5440a0c91585482e280fcd';
+
+function getPictures(query) {
+  //Creating serach parameters
+  const searchParams = new URLSearchParams({
+    key,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+
+  //Getting pictures
+  fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .then(data => {
+      const pictures = data.hits;
+      if (pictures.length === 0) {
+        iziToast.error({
+          title: 'No pictures found',
+          message: 'Try another query',
+          messageColor: 'black',
+          messageSize: '14px',
+          position: 'topCenter',
+          timeout: 3000,
+          closeOnClick: true,
+        });
+      }
+    })
+    .catch(e => console.log(e));
+}
+
+export default getPictures;
